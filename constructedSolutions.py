9e6e7524d8237eb1f73fcb049b4b4fcca780f7c6
truls <trulsmoholt@hotmail.com>
2020-12-27 16:00:27 +0100

    konvergenstest

--- constructedSolutions.py
+++ constructedSolutions.py
@@ -16,16 +16,16 @@ def difficult():
     print(sym.simplify(f))
     f = sym.lambdify([x,y,t],f)
 
-    equation = Richards()
+    equation = Richards(0.125)
     physics = equation.getPhysics()
     physics['source'] = f
     physics['neumann'] = sym.lambdify([x,y,t],K.subs(p,u_exact)*sym.diff(u_exact,y))
     mass,A,B,stiffness,source,u,error = FEM_solver(equation.geometry, equation.physics, initial = True)
 
-    th = np.linspace(0,1,10)
+    th = np.linspace(0,1,64)
 
     tau = th[1]-th[0]
-    TOL = 0.00005
+    TOL = 0.000005
     L = 3
 
     u = vectorize(u_exact.subs(t,0),equation.geometry)
@@ -37,7 +37,6 @@ def difficult():
         A = stiffness(K,u_j)
         lhs = L*B+tau*A
         u_j_n = np.linalg.solve(lhs,rhs)
-        print(np.linalg.norm(u_j_n-u_j))
 
         if np.linalg.norm(u_j_n-u_j)>TOL + TOL*np.linalg.norm(u_j_n):
             return newton(u_j_n,u_n,TOL,L,K,tau,f)
@@ -47,35 +46,36 @@ def difficult():
 
     for i in th[1:]:
         u =  newton(u,u,TOL,L,K,tau,source(i))
-        plot(u,equation.geometry)
         u_e = vectorize(u_exact.subs(t,i),equation.geometry)
-        plot(u_e,equation.geometry)
-        plot(u-u_e,equation.geometry)
         error.l2_error(u,u_exact.subs(t,i))
-
+    plot(u_e,equation.geometry)
+    plot(u-u_e,equation.geometry)
+    error.max_error(u,u_exact.subs(t,i))
 difficult()
 def heat_equation():
-    T_end = 2
+    T_end = 1
     f = sym.diff(u_exact,t,1)-(sym.diff(u_exact,x,2)+sym.diff(u_exact,y,2))
     f = sym.lambdify([x,y,t],f)
-    equation = Richards()
+    equation = Richards(0.0625)
     physics = equation.getPhysics()
     physics['source'] = f
     mass,A,B,stiffness,source,u,error = FEM_solver(equation.geometry, equation.physics, initial = True)
     u = vectorize(u_exact.subs(t,0),equation.geometry)
-    th = np.linspace(0,T_end,8)
+    th = np.linspace(0,T_end,256)
     tau = th[1]-th[0]
     for i in th[1:]:
         rhs = tau*source(i)+B@u
         lhs = tau*A+B
         u = np.linalg.solve(lhs,rhs)
         u_e = vectorize(u_exact.subs(t,i),equation.geometry)
-        error.l2_error(u,u_exact.subs(t,i))
+
 
     plot(u,equation.geometry)
     plot(u_e,equation.geometry)
 
     error.l2_error(u,u_exact.subs(t,T_end))
+    error.max_error(u,u_exact.subs(t,T_end))
+
 #heat_equation()
 
 
--- richards.py
+++ richards.py
@@ -2,67 +2,69 @@ import numpy as np
 import math
 from MeshGenerator import Mesh
 class Richards:
-    def __init__(self):
-        #Read input file
-        gmshfile = open("mesh/benchmark.msh",'r')
-        nodes = False
-        element = False
-        physicalname = False
-        coordinates = np.array([[0,0]])
-        elements = np.array([[0,0,0]])
-        boundary_elements_dirichlet = np.array([[0,0]])
-        boundary_elements_neumann = np.array([[0,0]])
-        neumannmarker = 0
-        dirichletmarker = 0
-        for line in gmshfile.readlines():
-            if line == '$Nodes\n':
-                nodes = True
-            if line == '$EndNodes\n':
-                nodes = False
-            if line == '$Elements\n':
-                element = True
-            if line == '$EndElements\n':
-                element = False
-            if line == '$PhysicalNames\n':
-                physicalname = True
-            if line == '$EndPhysicalNames\n':
-                physicalname = False
-            
-            if physicalname:
-                line_elements = line.split()
-                if len(line_elements)>=3:
-                    if line_elements[2]=='"NeumannBdry"':
-                        neumannmarker = int(line_elements[1])
-                    if line_elements[2]=='"DirichletBdry"':
-                        dirichletmarker = int(line_elements[1])
-            
+    def __init__(self,max_edge=False):
+        if not max_edge:
+            #Read input file
+            gmshfile = open("mesh/benchmark.msh",'r')
+            nodes = False
+            element = False
+            physicalname = False
+            coordinates = np.array([[0,0]])
+            elements = np.array([[0,0,0]])
+            boundary_elements_dirichlet = np.array([[0,0]])
+            boundary_elements_neumann = np.array([[0,0]])
+            neumannmarker = 0
+            dirichletmarker = 0
+            for line in gmshfile.readlines():
+                if line == '$Nodes\n':
+                    nodes = True
+                if line == '$EndNodes\n':
+                    nodes = False
+                if line == '$Elements\n':
+                    element = True
+                if line == '$EndElements\n':
+                    element = False
+                if line == '$PhysicalNames\n':
+                    physicalname = True
+                if line == '$EndPhysicalNames\n':
+                    physicalname = False
+                
+                if physicalname:
+                    line_elements = line.split()
+                    if len(line_elements)>=3:
+                        if line_elements[2]=='"NeumannBdry"':
+                            neumannmarker = int(line_elements[1])
+                        if line_elements[2]=='"DirichletBdry"':
+                            dirichletmarker = int(line_elements[1])
+                
 
-            #Creating list of coordinates
-            if nodes:
-                line_elements=line.split()
-                if len(line_elements)>=2:
-                    coordinates = np.concatenate((coordinates,np.array([[float(line_elements[1]),float(line_elements[2])]])))
+                #Creating list of coordinates
+                if nodes:
+                    line_elements=line.split()
+                    if len(line_elements)>=2:
+                        coordinates = np.concatenate((coordinates,np.array([[float(line_elements[1]),float(line_elements[2])]])))
+                
+                #Creating list of elements
+                if element:
+                    line_elements = line.split()
+                    if (len(line_elements) >= 2) and (line_elements[1]=='2'):
+                        elements = np.concatenate((elements,np.array([[int(line_elements[5])-1,int(line_elements[6])-1,int(line_elements[7])-1]])))
+                    if (len(line_elements) >= 2) and (line_elements[1]=='1') and (line_elements[3] == str(dirichletmarker)):
+                        boundary_elements_dirichlet = np.concatenate((boundary_elements_dirichlet,np.array([[int(line_elements[5])-1,int(line_elements[6])-1]])))
+                    if (len(line_elements) >= 2) and (line_elements[1]=='1') and (line_elements[3] == str(neumannmarker)):
+                        boundary_elements_neumann = np.concatenate((boundary_elements_neumann,np.array([[int(line_elements[5])-1,int(line_elements[6])-1]])))
+
             
-            #Creating list of elements
-            if element:
-                line_elements = line.split()
-                if (len(line_elements) >= 2) and (line_elements[1]=='2'):
-                    elements = np.concatenate((elements,np.array([[int(line_elements[5])-1,int(line_elements[6])-1,int(line_elements[7])-1]])))
-                if (len(line_elements) >= 2) and (line_elements[1]=='1') and (line_elements[3] == str(dirichletmarker)):
-                    boundary_elements_dirichlet = np.concatenate((boundary_elements_dirichlet,np.array([[int(line_elements[5])-1,int(line_elements[6])-1]])))
-                if (len(line_elements) >= 2) and (line_elements[1]=='1') and (line_elements[3] == str(neumannmarker)):
-                    boundary_elements_neumann = np.concatenate((boundary_elements_neumann,np.array([[int(line_elements[5])-1,int(line_elements[6])-1]])))
-
-        
-        coordinates=np.delete(coordinates,0,axis=0)
-        elements=np.delete(elements,0,axis=0)   
-        boundary_elements_dirichlet=np.delete(boundary_elements_dirichlet,0,axis=0)
-        boundary_elements_neumann=np.delete(boundary_elements_neumann,0,axis=0)
-
-        # mesh = Mesh(0.065*math.sqrt(2))
-        # boundary_elements_dirichlet = mesh.boundary_elements
-        # elements = mesh.elements
-        # coordinates = mesh.coordinates
+            coordinates=np.delete(coordinates,0,axis=0)
+            elements=np.delete(elements,0,axis=0)   
+            boundary_elements_dirichlet=np.delete(boundary_elements_dirichlet,0,axis=0)
+            boundary_elements_neumann=np.delete(boundary_elements_neumann,0,axis=0)
+        else: 
+            mesh = Mesh(max_edge*math.sqrt(2))
+            boundary_elements_dirichlet = mesh.boundary_elements
+            boundary_elements_neumann = np.array([[0,0]])
+            elements = mesh.elements
+            coordinates = mesh.coordinates
 
 
 
@@ -75,7 +77,7 @@ class Richards:
         def neumann(x,y,t):
             # need to multiply flux with K
             return 10 *t*x*(x - 1)
-        def dirichlet(x,z):
+        def dirichlet(x,z,t):
             return -1
 
         self.geometry = {
